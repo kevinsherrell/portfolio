1ee32ce4d0d5522d287d90687b49ffca3f3f05ce
kevinsherrell <k.sherrell.1028@gmail.com>
2019-05-05 15:55:03 -0500

    app now works in internet explorer

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// browser compatibility
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/stable'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
