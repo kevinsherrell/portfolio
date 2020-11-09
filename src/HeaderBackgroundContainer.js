87999e957a587a73d6d26ed3d3b959cb757002c9
kevinsherrell <k.sherrell.1028@gmail.com>
2020-11-08 19:53:50 -0700

    refactor

--- src/HeaderBackgroundContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class HeaderBackgroundContainer extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
-}
- 
-export default HeaderBackgroundContainer;
